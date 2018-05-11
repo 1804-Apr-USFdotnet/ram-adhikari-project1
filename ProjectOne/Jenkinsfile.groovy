5657310d0886c698b9c94d86c18ab665d5a69686
Ram Adhikari <ramadhikari96@gmail.com>
2018-05-11 13:51:41 -0400

    Added Jenkins config file

---
+++ ProjectOne/Jenkinsfile.groovy
@@ -0,0 +1,74 @@
+node('master') {
+    stage('import') {
+        try {
+            git 'https://github.com/1804-Apr-USFdotnet/ram-adhikari-project1.git'
+        }
+        catch (exc) {
+            slackError('import')
+            throw exc
+        }
+    }
+    stage('build') {
+        try {
+            dir('ram-adhikari-project1/ProjectOne') {
+                bat 'nuget restore'
+                bat 'msbuild /p:MvcBuildViews=true'
+            }
+        }
+        catch (exc) {
+            slackError('build')
+            throw exc
+        }
+    }
+    stage('test') {
+        try {
+            dir('ram-adhikari-project1/ProjectOne') {
+                bat "VSTest.Console TestDriven\\bin\\Debug\\TestDriven.dll"
+            }
+        }
+        catch (exc) {
+            slackError('test')
+            throw exc
+        }
+    }
+    stage('analyze') {
+        try {
+            dir('ram-adhikari-project1/ProjectOne') {
+                bat 'SonarScanner.MSBuild begin /k:restaurant_project1_ram /v:0.1.0 /d:sonar.verbose=true'
+                bat 'msbuild /t:rebuild'
+                bat 'SonarScanner.MSBuild end'
+            }
+        }
+        catch (exc) {
+            slackError('analyze')
+            throw exc
+        }
+    }
+    stage('package') {
+        try {
+            dir('ram-adhikari-project1/ProjectOne') {
+                bat 'msbuild ProjectOne.PL /t:package /p:Configuration=Debug;PackageFileName=..\\Package.zip'
+            }
+        }
+        catch (exc) {
+            slackError('package')
+            throw exc
+        }
+    }
+    stage('deploy') {
+        try {
+            dir('ram-adhikari-project1/ProjectOne') {
+                bat "msdeploy -verb:sync -source:package=\"%CD%\\Package.zip\" -dest:auto,computerName=\"https://ec2-18-191-64-122.us-east-2.compute.amazonaws.com:8172/msdeploy.axd\",userName=\"Administrator\",password=\"${env.Deploy_Password}\",authtype=\"basic\",includeAcls=\"False\" -disableLink:AppPoolExtension -disableLink:ContentExtension -disableLink:CertificateExtension -setParam:\"IIS Web Application Name\"=\"Default Web Site/ProjectOne\" -enableRule:AppOffline -allowUntrusted"
+            }
+        }
+        catch (exc) {
+            slackError('deploy')
+            throw exc
+        }
+    }
+}
+
+def slackError(stageName) {
+    slackSend color: 'danger', message: "FAILED ${stageName} stage: [<${JOB_URL}|${env.JOB_NAME}> <${env.BUILD_URL}console|${env.BUILD_DISPLAY_NAME}>] [${currentBuild.durationString.replace(' and counting', '')}]"
+}
+      
