17dd8f2efbbedbb2a412371a3d44e627b4faf20c
Ram Adhikari <ramadhikari96@gmail.com>
2018-05-11 13:32:15 -0400

    Build succeeded

--- ProjectOne/WebApp/Controllers/HomeController.cs
+++ ProjectOne/WebApp/Controllers/HomeController.cs
@@ -13,12 +13,14 @@ namespace WebApp.Controllers
             return View();
         }
 
+        
         public ActionResult About()
         {
             ViewBag.AboutMessage = "This website is currently under construction; however, when it's done, it will provide " +
                                 "information realted to restaurants and the quality of food they provide.";
 
             return View();
+            
         }
     }
 }
